c7969aafbf198bdabe93c28a8e89220b73e1a526
Dögös Robi <pacsa.alex@diak.szbi-pg.hu>
2026-01-15 10:44:21 +0100

    szia tecel

---
+++ valtozo_tipusok.js
@@ -0,0 +1,21 @@
+console.log(typeof(2));
+console.log(typeof(true));
+console.log(typeof('Aladár'));
+console.log(typeof ({name: 'Béla', age: 42}));
+console.log(typeof(function f(a, b) {return a + b}));
+console.log(typeof(nonexisting));
+
+let a = 0.1 + 0.2
+console.log(a)
+console.log(a.toFixed(2))
+console.log(typeof(a.toFixed(2)))
+
+let firstName = 'John' 
+let LastName = 'Johnson'
+
+console.log(' Hello ' + firstName  + ' ' + LastName)
+console.log(`Hello ${firstName} ${LastName}`)
+
+let kocka = "apu"
+
+let has = "test"
---
+++ valtozok.js
@@ -0,0 +1,12 @@
+console.log("Hello!")
+var a = 2;
+console.log(a)
+
+var b = "asd"
+console.log(b)
+
+let b = 3;
+b = 4;
+const c = 2;
+
+
